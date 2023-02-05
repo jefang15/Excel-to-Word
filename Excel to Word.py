f5ef4b35a54100f2c74f2d95e67accf008a8ea20
jefang15 <jeffreyfang@msn.com>
2023-02-05 11:23:23 -0500

    Create Excel to Word.py

---
+++ Excel to Word.py
@@ -0,0 +1,17 @@
+
+
+import pandas as pd
+import numpy as np
+from tabulate import tabulate
+import docx
+from docx import Document
+from docx.oxml import OxmlElement, ns
+from docx.shared import Inches
+from docx.shared import RGBColor
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+import glob
+
+""" ########################################################################################################################## """
+""" Import Survey Responses """
+""" ########################################################################################################################## """
+
